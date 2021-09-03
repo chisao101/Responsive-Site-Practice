000e55e4a3212e44910698ccc4b787d4f2c0f976
Mike Nealon <chisao101@gmail.com>
2021-09-03 16:43:36 -0400

    create and style wrapper div

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+const body = document.querySelector('body'); // get body from html document
+body.style.backgroundColor = '#ddd'; // body background color light grey
+
+// create and style wrapper div
+const mainWrap = document.createElement('div'); //create div
+body.appendChild(mainWrap); // append to body
+mainWrap.classList.add('main-wrap'); // add class
+mainWrap.style.backgroundColor = '#fff'; // background color white
+mainWrap.style.width = '90vw'; // width 90% of viewport width
+mainWrap.style.margin = '0 auto'; // center on page
+mainWrap.style.padding = '2rem'; // add padding
+
+
