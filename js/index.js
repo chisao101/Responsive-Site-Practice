2940d13fa62d668b8ef4a4a84172aa469f750d90
Mike Nealon <chisao101@gmail.com>
2021-09-03 22:02:36 -0400

    added header, logo, and started nav menu

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ body.style.backgroundColor = '#ddd'; // body background color light grey
 // create and style wrapper div
 const mainWrap = document.createElement('div'); //create div
 body.appendChild(mainWrap); // append to body
-mainWrap.classList.add('main-wrap'); // add class
+// mainWrap.classList.add('main-wrap'); // add class
 mainWrap.style.backgroundColor = '#fff'; // background color white
 mainWrap.style.width = '90vw'; // width 90% of viewport width
 mainWrap.style.margin = '0 auto'; // center on page
@@ -14,3 +14,30 @@ mainWrap.style.padding = '2rem'; // add padding
 const header = document.createElement('header');
 mainWrap.appendChild(header);
 header.style.display = 'flex';
+
+// create and style logo and div
+const logoContainer = document.createElement('div');
+header.appendChild(logoContainer);
+// logo h1 element
+const logo = document.createElement('h1');
+header.appendChild(logo);
+logo.style.fontFamily = '"Poppins", sans-serif';
+const logoFirstWord = document.createElement('span');
+logoFirstWord.innerText = 'Flex';
+const logoSecondWord = document.createElement('span');
+logoSecondWord.innerText = 'Box';
+logo.appendChild(logoFirstWord);
+logo.appendChild(logoSecondWord);
+logoFirstWord.style.fontWeight = '700';
+logoSecondWord.style.fontWeight = '200';
+
+
+// create and style nav menu
+const navMenu = document.createElement('nav');
+header.appendChild(navMenu);
+const navLink =
+
+
+
+
+// create and style register button and div
